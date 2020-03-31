02a63f68742a14755c6105d26722b62f9d073c2f
KhizerHumayun <60516459+KhizerHumayun@users.noreply.github.com>
2020-03-31 01:12:56 -0400

    Update mainPage.js

--- Components/mainPage.js
+++ Components/mainPage.js
@@ -40,7 +40,7 @@ export default class App extends Component<{}> {
                     title='TakeAway'
                     color='#f04b48' 
                 />
-            </TouchableOpacity >  
+            </TouchableOpacity>  
 
       </View>
     );
